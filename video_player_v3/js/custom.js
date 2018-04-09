f9a7fe473c4fd50ccca3a777dc205dd7cad0ca24
Kaung Lwin <klwin@email.arizona.edu>
2018-04-08 22:39:38 -0700

    final js

--- video_player_v3/js/custom.js
+++ video_player_v3/js/custom.js
@@ -3,73 +3,65 @@ $('video').mediaelementplayer({
 			startLanguage: 'en'
 		});
 		const vid = document.getElementById("main-video");
+
+		function setTimes(start, end=59) {
+			$('span').css('color','black');
+			return (vid.currentTime >= start && vid.currentTime < end);
+		}
+
+		function changeColor(id) {
+			$(id).css('color','orange');
+		}
+
 		let timeNow = () => {
 			setInterval(() => {
-				if(vid.currentTime >= 0.240 && vid.currentTime < 4.130) {
-					$('.one').css('color','orange');
+				if(setTimes(0.240, 4.130)) {
+					changeColor('.one');
 				}
-				else if(vid.currentTime >= 4.130 && vid.currentTime < 7.535) {
-					$('span').css('color','black');
-					$('.two').css('color','orange');
+				else if(setTimes(4.130, 7.535)) {
+					changeColor('.two');
 				}
-				else if(vid.currentTime >= 7.535 && vid.currentTime < 11.270) {
-					$('span').css('color','black');
-					$('.three').css('color','orange');
+				else if(setTimes(7.535, 11.270)) {
+					changeColor('.three');
 				}
-				else if(vid.currentTime >= 11.270 && vid.currentTime < 13.960) {
-					$('span').css('color','black');
-					$('.four').css('color','orange');
+				else if(setTimes(11.270, 13.960)) {
+					changeColor('.four');
 				}
-				else if(vid.currentTime >= 13.960 && vid.currentTime < 17.940) {
-					$('span').css('color','black');
-					$('.five').css('color','orange');
+				else if(setTimes(13.960, 17.940)) {
+					changeColor('.five');
 				}
-				else if(vid.currentTime >= 17.940 && vid.currentTime < 22.370) {
-					$('span').css('color','black');
-					$('.six').css('color','orange');
+				else if(setTimes(17.940, 22.370)) {
+					changeColor('.six');
 				}
-				else if(vid.currentTime >= 22.370 && vid.currentTime < 26.880) {
-					$('span').css('color','black');
-					$('.seven').css('color','orange');
+				else if(setTimes(22.370, 26.880)) {
+					changeColor('.seven');
 				}
-				else if(vid.currentTime >= 26.880 && vid.currentTime < 30.920) {
-					$('span').css('color','black');
-					$('.eight').css('color','orange');
+				else if(setTimes(26.880, 30.920)) {
+					changeColor('.eight');
 				}
-				else if(vid.currentTime >= 32.100 && vid.currentTime < 34.730) {
-					$('span').css('color','black');
-					$('.nine').css('color','orange');
+				else if(setTimes(32.100, 34.730)) {
+					changeColor('.nine');
 				}
-				else if(vid.currentTime >= 34.730 && vid.currentTime < 39.430) {
-					$('span').css('color','black');
-					$('.ten').css('color','orange');
+				else if(setTimes(34.730, 39.430)) {
+					changeColor('.ten');
 				}
-				else if(vid.currentTime >= 39.430 && vid.currentTime < 41.190) {
-					$('span').css('color','black');
-					$('.eleven').css('color','orange');
+				else if(setTimes(39.430, 41.190)) {
+					changeColor('.eleven');
 				}
-				else if(vid.currentTime >= 41.190 && vid.currentTime < 46.300) {
-					$('span').css('color','black');
-					$('.twelve').css('color','orange');
+				else if(setTimes(41.190, 46.300)) {
+					changeColor('.twelve');
 				}
-				else if(vid.currentTime >= 46.300 && vid.currentTime < 49.270) {
-					$('span').css('color','black');
-					$('.thirteen').css('color','orange');
+				else if(setTimes(46.300, 49.270)) {
+					changeColor('.thirteen');
 				}
-				else if(vid.currentTime >= 49.270 && vid.currentTime < 53.760) {
-					$('span').css('color','black');
-					$('.fourteen').css('color','orange');
+				else if(setTimes(49.270, 53.760)) {
+					changeColor('.fourteen');
 				}
-				else if(vid.currentTime >= 53.760 && vid.currentTime < 57.780) {
-					$('span').css('color','black');
-					$('.fifteen').css('color','orange');
+				else if(setTimes(53.760, 57.780)) {
+					changeColor('.fifteen');
 				}
-				else if(vid.currentTime >= 57.780) {
-					$('span').css('color','black');
-					$('.sixteen').css('color','orange');
-				}
-				else {
-					$('span').css('color','black');
+				else if(setTimes(57.780)) {
+					changeColor('.sixteen');
 				}
 			}, 100)
 		}
